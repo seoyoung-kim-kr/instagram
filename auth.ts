99e74472578fd244dbee4f79b2dbb3d80129bcb5
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-23 17:49:56 +0900

    feat : NextAuth session 리턴 시, username (이메일 @앞부분) 추가 리턴하도록 설정

--- auth.ts
+++ auth.ts
@@ -3,4 +3,16 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    async session({ session }) {
+      const user = session?.user;
+      if (user) {
+        session.user = {
+          ...user,
+          username: user.email?.split("@")[0],
+        };
+      }
+      return session;
+    },
+  },
 });
