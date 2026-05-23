da5cc5327ced00bbd667e359db8178fed512aadb
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-23 23:47:36 +0900

    refactor: schemaType에 prettier 적용

--- sanity/schemaTypes/post.js
+++ sanity/schemaTypes/post.js
@@ -1,66 +1,63 @@
-import { Preview } from 'sanity';
-
 export default {
-  title: 'Post',
-  name:'post',
-  type: 'document',
+  title: "Post",
+  name: "post",
+  type: "document",
   fields: [
     {
-      title: 'Author',
-      name: 'author',
-      type: 'reference',
-      to: [{ type: 'user' }]
-
+      title: "Author",
+      name: "author",
+      type: "reference",
+      to: [{ type: "user" }],
     },
     {
-      title: 'Photo',
-      name: 'photo',
-      type: 'image'
+      title: "Photo",
+      name: "photo",
+      type: "image",
     },
     {
-      title: 'Likes',
-      name: 'likes',
-      type: 'array',
+      title: "Likes",
+      name: "likes",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'user' }]
-        }
+          type: "reference",
+          to: [{ type: "user" }],
+        },
       ],
-      validation: (Rule) => Rule.unique()
+      validation: (Rule) => Rule.unique(),
     },
     {
-      title: 'Comments',
-      name: 'comments',
-      type: 'array',
+      title: "Comments",
+      name: "comments",
+      type: "array",
       of: [
         {
-          title: 'Comment',
-          name: 'comment',
-          type: 'document',
+          title: "Comment",
+          name: "comment",
+          type: "document",
           fields: [
             {
-              title:'Author',
-              name: 'author',
-              type: 'reference',
-              to:[{type: 'user'}]
+              title: "Author",
+              name: "author",
+              type: "reference",
+              to: [{ type: "user" }],
             },
             {
-              title:'Comment',
-              name: 'comment',
-              type: 'string'
-            }
-          ]
-        }
-      ]
-    }
+              title: "Comment",
+              name: "comment",
+              type: "string",
+            },
+          ],
+        },
+      ],
+    },
   ],
   preview: {
     select: {
-      title: 'comments.0.comment',
-      authorName:'author.name',
-      authorUsername: 'author.username',
-      media:'photo'
+      title: "comments.0.comment",
+      authorName: "author.name",
+      authorUsername: "author.username",
+      media: "photo",
     },
     prepare(selection) {
       const { title, authorName, authorUsername, media } = selection;
@@ -68,7 +65,7 @@ export default {
         title,
         subtitle: `by ${authorName}(${authorUsername})`,
         media,
-      }
-    }
-  }
-}
+      };
+    },
+  },
+};
