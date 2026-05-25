55b0fbd7c7fe25e0d6575a64f579e293464d232e
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-25 16:04:26 +0900

    feat: HomePage 골격 구현

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import { auth } from "@/auth";
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+
+export default async function Homepage() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) return null;
+
   return (
-     <h1>Instagram</h1>
+    <section className="w-full max-w-212.5 flex flex-col md:flex-row justify-between gap-x-10 px-10 py-7">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
   );
 }
