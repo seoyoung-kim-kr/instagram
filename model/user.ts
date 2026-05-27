8f0b5f7e18d3201c00313b6d5ea4e3805a04346f
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-27 20:55:16 +0900

    feat: DetailUser, SimpleUser 타입 추가

--- model/user.ts
+++ model/user.ts
@@ -4,3 +4,12 @@ export type User = {
   email: string;
   image?: string;
 };
+
+export type SimpleUser = Pick<User, "username" | "image">;
+
+// User + Detail field
+export type DetailUser = User & {
+  following: SimpleUser[];
+  followers: SimpleUser[];
+  bookmarks: string[];
+};
