4c72c71aefef9bcec5b0e41e600cfe8c5e4097d7
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-27 20:32:27 +0900

    design: sideBar 스타일링 수정

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,19 +9,21 @@ export default function SideBar({ user: { name, username, image } }: Props) {
   return (
     <section className="space-y-7">
       <div className="flex gap-x-4 items-center">
-        {image && <Avatar image={image} size="lg" highlight={false} />}
+        {image && <Avatar image={image} size="md" highlight={false} />}
         <div>
           <p className="font-semibold">{username}</p>
           <p className="text-neutral-500 leading-4">{name}</p>
         </div>
       </div>
 
-      <p className="text-neutral-500">
+      <p className="text-neutral-500 text-sm">
         About ∙ Help ∙ Press ∙ API ∙ Jobs ∙ Privacy ∙ Terms ∙ Location ∙
         Language
       </p>
 
-      <p className="font-bold text-gray-500">@Copyright INSTAGRAM from METAL</p>
+      <p className="font-bold text-gray-500 text-sm">
+        @Copyright INSTAGRAM from METAL
+      </p>
     </section>
   );
 }
