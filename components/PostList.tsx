7d52db45c43816c88612ec7f7a19bff410678f9f
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-05-31 15:55:44 +0900

    design: Loading 스피너 가운데로 오도록 스타일링

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -2,15 +2,19 @@
 
 import useSWR from "swr";
 import { Spinner } from "./ui/Spinner";
-import type { Post as PostType } from "@/model/post";
-import Post from "./Post";
+import type { SimplePost } from "@/model/post";
+import Post from "./PostListCard";
 
 export default function PostList() {
-  const { data: posts, isLoading, error } = useSWR<PostType[]>("/api/posts");
+  const { data: posts, isLoading, error } = useSWR<SimplePost[]>("/api/posts");
 
   return (
     <section className="w-full mt-6">
-      {isLoading && <Spinner />}
+      {isLoading && (
+        <div className="flex justify-center items-center mt-32">
+          <Spinner />
+        </div>
+      )}
       {error && <p>error: {error.message}</p>}
       {posts && posts.length > 0 && (
         <ul className="space-y-5">
