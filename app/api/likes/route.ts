93f0a9ebf7910ae276127444396c40363d7679bd
Seoyoung Kim <seoyoung.k.kr@gmail.com>
2026-07-07 21:07:51 +0900

    feat: PUT likes API 추가

---
+++ app/api/likes/route.ts
@@ -0,0 +1,24 @@
+import { auth } from "@/auth";
+import { dislikePost, likePost } from "@/service/post";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Required", { status: 401 });
+  }
+
+  const { id, like } = await req.json();
+
+  if (!id || like === undefined) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const request = like ? likePost : dislikePost;
+
+  return request(id, user.id)
+    .then((res) => NextResponse.json(res))
+    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+}
